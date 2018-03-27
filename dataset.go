3f1827683016f2d406b6201d3893e157bf03c889
Caleb Phillips <calebrphillips@gmail.com>
2018-03-27 09:48:54 -0400

    Sort Dataset type by key

--- dataset.go
+++ dataset.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"sort"
+)
+
 type Dataset struct {
 	x []float64
 	y []float64
@@ -9,6 +13,7 @@ func NewDataset(x []int64, y []int64) *Dataset {
 	d := new(Dataset)
 	d.x = convertSliceIntToFloat(x)
 	d.y = convertSliceIntToFloat(y)
+	sort.Sort(d)
 	return d
 }
 
@@ -17,4 +22,15 @@ func convertSliceIntToFloat(l []int64) (r []float64) {
 		r = append(r, float64(num))
 	}
 	return
+}
+
+func (d *Dataset) Len() int {
+	return len(d.x)
+}
+func (d *Dataset) Swap(i, j int) {
+	d.x[i], d.x[j] = d.x[j], d.x[i]
+	d.y[i], d.y[j] = d.y[j], d.y[i]
+}
+func (d *Dataset) Less(i, j int) bool {
+	return d.x[i] < d.x[j]
 }
